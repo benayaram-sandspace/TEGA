d0b83c7036a4a5122f35290284de4afa6b78b2f0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-09-17 18:34:53 +0530

    modified:   tega/lib/pages/college_screens/dashboard/more_options/student_learning_model.dart

--- tega/lib/pages/college_screens/dashboard/more_options/student_learning_model.dart
+++ tega/lib/pages/college_screens/dashboard/more_options/student_learning_model.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
 
-// class Student {
-//   final String name;
-//   final int grade;
-//   final double gpa;
-//   final String avatarUrl;
-//   final String status;
-//   final Color statusColor;
-
-//   const Student({
-//     required this.name,
-//     required this.grade,
-//     required this.gpa,
-//     required this.avatarUrl,
-//     required this.status,
-//     required this.statusColor,
-//   });
-// }
-
 class Activity {
   final String title;
   final String type;
